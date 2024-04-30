60ace737a887d48be64802f6dd09b60b3d30fa96
Chris <chriscave89@gmail.com>
2024-04-30 18:50:05 +0200

    standardise colors to hex and color map returns dict of Char instances

--- mpl_ascii/color_map.py
+++ mpl_ascii/color_map.py
@@ -1,3 +1,4 @@
+import matplotlib
 from matplotlib.collections import PathCollection
 from matplotlib.container import BarContainer
 from matplotlib.patches import Rectangle
@@ -18,7 +19,7 @@ bar_chars = [
     "~"
 ]
 
-line_char = [
+line_chars = [
     "+",
     "o",
     "a",
@@ -27,7 +28,6 @@ line_char = [
     "d",
     "y",
     "~",
-    "!"
 ]
 
 scatter_chars = [
@@ -39,7 +39,19 @@ scatter_chars = [
     "n",
 ]
 
-def map_color_to_ascii(ax):
+class Char:
+    def __init__(self, character: str, color: str) -> None:
+        self.character=character
+        self.color=color
+
+    def __str__(self) -> str:
+        return self.character
+
+
+    def __rich__(self) -> str:
+        return f"[{self.color}]{self.character}[/{self.color}]"
+
+def ax_color_map(ax):
 
     def ascii_chars(ls):
         index = 0
@@ -55,27 +67,31 @@ def map_color_to_ascii(ax):
         for bar in container.patches:
             if not isinstance(bar, Rectangle):
                 continue
-            color = bar.get_facecolor()
+            color = std_color(bar.get_facecolor())
             if color in color_to_ascii:
                 continue
-            color_to_ascii[color] = next(gen)
+            color_to_ascii[color] = Char(next(gen), color)
 
     lines = ax.get_lines()
-    gen = ascii_chars(line_char)
+    gen = ascii_chars(line_chars)
     for line in lines:
-        color = line.get_color()
+        color = std_color(line.get_color())
         if color in color_to_ascii:
             continue
-        color_to_ascii[color] = next(gen)
+        color_to_ascii[color] = Char(next(gen), color)
 
 
-    gen = ascii_chars(scatter_char)
+    gen = ascii_chars(scatter_chars)
     for collection in ax.collections:
         if not isinstance(collection, PathCollection):
             continue
         for color in collection.get_facecolor():
-            color = tuple(color.tolist())
+            color = std_color(tuple(color.tolist()))
             if color in color_to_ascii:
                 continue
-            color_to_ascii[color] = next(gen)
+            color_to_ascii[color] = Char(next(gen), color)
+
     return color_to_ascii
+
+def std_color(color):
+    return matplotlib.colors.to_hex(color)
